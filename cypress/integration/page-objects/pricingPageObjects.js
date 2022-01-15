1c2ac23dcd1de9efb5bcf041452c72cc4c7d3921
moocallteen <kukarinworkspaces@gmail.com>
2022-01-16 01:10:48 +0200

    second, third and additional scenarios implementation

--- cypress/integration/page-objects/pricingPageObjects.js
+++ cypress/integration/page-objects/pricingPageObjects.js
@@ -1,10 +1,17 @@
 module.exports = {
   //CSS selectors
   yearlyPeriodBtn: 'li[data-price-period="2"]',
+  twoYearsPeriodBtn: 'li[data-price-period="3"]',
   rentalNumberInput: 'input[id="scroll-prop-plan"]',
   starterInfoCard: 'div[class$="card-starter"]',
+  currencySelector: 'select[class^="price-currency"]',
+  sliderRound: 'div[class="slider-handle min-slider-handle round"]',
 
   //XPath selectors
   proInfoCard: '//h6[text()="Professional"]/../../../div',
   ultimateInfoCard: '//h6[text()="Ultimate"]/../../../div',
+  faqQest01:
+    '//div[contains(@class, "faq-item")][1]//div[@class="faq-question"]',
+  faqAnwer01:
+    '//div[contains(@class, "faq-item")][1]//div[@class="faq-answer"]',
 };
